4571380961aeb0a059e18c2d368838cbb290bdb7
Darran Kelinske <darran.kelinske@gmail.com>
2020-03-04 14:29:46 -0600

    Further work on mulitplatform

---
+++ js/src/main/kotlin/index.kt
@@ -0,0 +1,27 @@
+package com.fivestars.cordovaalternativepattern
+
+import org.w3c.dom.MessageChannel
+import org.w3c.dom.MessageEvent
+import kotlin.browser.window
+
+
+fun configureChannel() {
+
+    val channel = MessageChannel()
+    var jsPortOne = channel.port1
+    var jsPortTwo = channel.port2
+
+    window.addEventListener("message", {
+    }, false)
+
+    jsPortOne.addEventListener("message", {
+        window.alert((it as MessageEvent).data.toString())
+    }, false)
+
+    jsPortTwo.addEventListener("message", {
+        window.alert((it as MessageEvent).data.toString())
+    }, false)
+
+    jsPortOne.start()
+    jsPortTwo.start()
+}
